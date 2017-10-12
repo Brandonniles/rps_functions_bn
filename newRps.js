759886274ff0b168d67d51a979269fb566931af5
Brandon O. Niles <b21niles@gmail.com>
2017-10-12 18:38:30 -0400

    final version

--- newRps.js
+++ newRps.js
@@ -9,7 +9,6 @@ function getHand2() {
   var raN = Math.floor(Math.random() * 3);
   return hands[raN];
 }
-
 // vv two players
 var p1 = { name: 'Brandon'};
 var p2 = { name: 'Ashley'};
@@ -37,10 +36,12 @@ function playRound(player1, player2) {
 function playGame(player1, player2, playUntil) {
   var scoreP1 = 0;
   var scoreP2 = 0;
-  var rn = getHand(); //p1's random throw
-  var rn2 = getHand2(); //p2's random throw
+
 
   while ((scoreP1 < playUntil) && (scoreP2 < playUntil)) {
+
+    var rn = getHand(); //p1's random throw
+    var rn2 = getHand2(); //p2's random throw
     if (rn === rn2) {
     console.log(`${p1.name} played: ${rn} and ${p2.name} played: ${rn2}`);
     console.log("It's a tie!");
@@ -67,4 +68,4 @@ function playGame(player1, player2, playUntil) {
     console.log(`${p2.name} won the game`);
   }
 
-}
+} 
